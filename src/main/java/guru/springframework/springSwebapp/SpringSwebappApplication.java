ed93b7152d470a1d5c1eb6852d60a1224427f933
Dion Kadriu <dik00028@students.stir.ac.uk>
2021-03-05 11:28:04 +0100

    show commit

--- src/main/java/guru/springframework/springSwebapp/SpringSwebappApplication.java
+++ src/main/java/guru/springframework/springSwebapp/SpringSwebappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSwebappApplication {
+public class SpringSwebappApplication { //just to check that commit is made
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSwebappApplication.class, args);
